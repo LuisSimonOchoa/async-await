3461d03e5a2ca7aa77dc67107396dd57ef19a451
LuisSimonOchoa <lsimonochoa@gmail.com>
2024-03-31 14:40:11 -0500

    Add a new example

---
+++ async-await.js
@@ -0,0 +1,12 @@
+async function requesHandler(req, res) {
+    try {
+        const user = await User.findById(req.userId);
+        const tasks = await Tasks.findById(user.tasksId);
+        tasks.completed = true;
+        await tasks.save();
+        res.send('Task Completed');
+    }
+    catch(e) {
+        res.send(e);
+    }
+}
